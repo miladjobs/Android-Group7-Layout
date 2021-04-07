c1c1dc2e3d58c8c4d80560526ce8ea85eb208153
Milad Esfandiyari <miladisjobs@gmail.com>
2021-04-07 16:14:48 +0430

    modify popular foods

--- app/src/main/java/com/example/group7_ui_app/MainActivity.kt
+++ app/src/main/java/com/example/group7_ui_app/MainActivity.kt
@@ -3,11 +3,29 @@ package com.example.group7_ui_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_hotdog = findViewById<ImageButton>(R.id.imageButton3)
+        btn_hotdog.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked hot dog.", Toast.LENGTH_SHORT).show()
+        }
+        val btn_sushi = findViewById<ImageButton>(R.id.imageButton4)
+        btn_sushi.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked sushi.", Toast.LENGTH_SHORT).show()
+        }
+        val btn_pizza = findViewById<ImageButton>(R.id.imageButton5)
+        btn_pizza.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked pizza.", Toast.LENGTH_SHORT).show()
+        }
+        val btn_more = findViewById<ImageButton>(R.id.imageButton6)
+        btn_more.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked more.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -37,5 +55,7 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
+
+
 }
 
